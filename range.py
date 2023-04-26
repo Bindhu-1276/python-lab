e080fd6009b5078fafaf7469f8e1272af3b9555e
Bindhu-1276 <121797629+Bindhu-1276@users.noreply.github.com>
2023-04-26 15:07:22 +0530

    range.py

---
+++ range.py
@@ -0,0 +1,14 @@
+ n=int(input("enter the number"))
+ if n>0 and n<=100:
+   print(n,"is there")
+ else:
+   print("the number is not found")
+
+
+
+#output:enter the number 23
+23 is there
+enter the number122
+the number is not found
+enter the number444
+the number is not found
