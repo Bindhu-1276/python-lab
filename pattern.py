2d9911e01a5d58643e2fc833af1f3a94fa78c97c
Bindhu-1276 <121797629+Bindhu-1276@users.noreply.github.com>
2023-04-26 14:41:06 +0530

    pattern.py

---
+++ pattern.py
@@ -0,0 +1,17 @@
+n=int(input("enter the number"))
+for i in range(0,n):
+  for j in range(0,i+1):
+    print(n-i,end=" ")
+  print("\n")
+  
+  
+  #output:enter the number5
+5 
+
+4 4 
+
+3 3 3 
+
+2 2 2 2 
+
+1 1 1 1 1 
