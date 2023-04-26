befa6be9ea6eef7e79cda03ab5f0aea61ba65a56
Bindhu-1276 <121797629+Bindhu-1276@users.noreply.github.com>
2023-04-26 15:01:48 +0530

    grades.py

---
+++ grades.py
@@ -0,0 +1,18 @@
+
+ n=int(input("enter the number"))
+ if n>80:
+   print(n,"o grade")
+ elif n<=80 and n>=65 :
+   print(n,"A grade")
+ elif n<=65 and n>=50 :
+   print(n,"B grade")
+ elif n<=50 and n>=40 :
+   print (n, " c grade")
+ else:
+   print("fail")
+   
+   
+   #output:enter the number 80
+80 A grade
+enter the number45
+45  c grade
