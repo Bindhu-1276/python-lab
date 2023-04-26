24c1489c11d5fd3b643b5d2f261eee7c4170acf3
Bindhu-1276 <121797629+Bindhu-1276@users.noreply.github.com>
2023-04-26 14:47:45 +0530

    uppercase.py

---
+++ uppercase.py
@@ -0,0 +1,20 @@
+s=input("enter the characters")
+if s>='A' and s<='Z':
+  print(s,"uppercase")
+elif s>='a' and s<='z':
+  print(s,"lowercase")
+elif s>="0" and s<="9":
+  print(s,"all the digits")
+else:
+  print("special chracters")
+  
+  
+  
+#output:  enter the charactersA
+A uppercase
+ enter the charactersR
+R uppercase
+
+ enter the charactersb
+b lowercase
+
