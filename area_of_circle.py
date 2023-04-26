dc09d4d2e55833ad27ff768ca30cacbe418d1a91
Bindhu-1276 <121797629+Bindhu-1276@users.noreply.github.com>
2023-04-26 14:08:51 +0530

    area_of_circle.py

---
+++ area_of_circle.py
@@ -0,0 +1,10 @@
+import math
+  2 r=int(input("enter the radius of the circle"))
+  3 area=math.pi*r*r
+  4 print("area of circle is {}".format(area))
+  
+  
+  
+  
+  #output:enter the radius of the circle3
+area of circle is 28.274333882308138
