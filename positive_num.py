4ae656d5acf733be55b48c11764f4928f48b663e
Bindhu-1276 <121797629+Bindhu-1276@users.noreply.github.com>
2023-04-26 14:52:06 +0530

    positive_num.py

---
+++ positive_num.py
@@ -0,0 +1,14 @@
+ n=int(input("enter the number"))
+ if n>0:
+  print(n,"the number is positive")
+ elif n<0:
+  print(n,"the number is negative")
+ else:
+   print("the number is 0")
+
+
+
+#output:enter the number -5
+-5 the number is negative
+enter the number 4
+4 the number is positive
